98efdda3e21e99ea029aa81344c1c378a802650e
hongrong <ghrace@live.com>
2018-11-13 15:10:22 +0800

    1113

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div ref="usernmae">{{ userName }}</div>
+    <button @click="handleChangeName">click</button>
   </div>
 </template>
+<script lang="ts">
+import {Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class About extends Vue {
+  private userName: string = "abc";
+
+  handleChangeName():void {
+    this.userName = "cde";
+    console.log(this.$refs.usernmae.innerText)
+  }
+}
+</script>
