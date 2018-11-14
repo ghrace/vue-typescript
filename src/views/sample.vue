bf3c7bfa99638990e27d8544e5c208bca59bd62a
hongrong <ghrace@live.com>
2018-11-14 18:16:20 +0800

    1114

---
+++ src/views/sample.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <input type="text" v-model="user">
+        <p>{{user}}</p>
+    </div>
+</template>
+
+<script lang="ts">
+ import { Component, Vue, Model, Watch, Prop } from 'vue-property-decorator';
+
+ @Component
+ export default class Sample extends Vue {
+     public user:string=''
+     @Prop() private name !:string;
+    
+     created():void {
+         console.log(1)
+     }
+
+     @Watch ('$route')
+     onRouteChanged(route:any,old:any):void {
+         console.log(1111)
+         console.log(route);
+     } 
+     @Watch ('user')
+     onUserChangeed(newVal:string,oldVal:string):void{
+         console.log(newVal,oldVal)
+     }
+
+ }
+</script>
+<style>
+
+
+</style>
